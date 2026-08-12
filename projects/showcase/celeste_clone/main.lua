8f494296ea57074687f43c2ff8b12c7f7ac455d9
Andrej Markuš <andrejmarkus274@gmail.com>
2026-08-12 22:03:14 +0200

    feat(showcase): scaffold platformer project

---
+++ projects/showcase/celeste_clone/main.lua
@@ -0,0 +1,11 @@
+function _init()
+  set_palette_color(0, 100, 160, 230) -- sky blue, placeholder until Task 3
+end
+
+function _update()
+end
+
+function _draw()
+  clear_screen()
+  draw_text("CELESTE CLONE - BOOT OK", 4, 60, 7)
+end
